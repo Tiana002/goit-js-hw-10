fc35e6afe3a3d1606d332583044cedc030b6aefd
Tetiana Blinova <tiana002@gmail.com>
2023-04-21 16:55:01 +0300

    fix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,10 @@
- 
+
 
 export default function fetchCountries(name) {
-    const URL = 'https://restcountries.com/v3.1/name/';
-   // const FIELDS = 'name,capital,population,flags,languages'; 
+    const URL_BASE = 'https://restcountries.com/v3.1/name/';
+const FIELDS = 'name,capital,population,flags,languages'; 
    
-    return fetch('${URL}${name}?fields=name,capital,population,flags,languages')
+    return fetch('${URL_BASE}${name}?fields=${FIELDS}')
     .then(response => {
         if (!response.ok) {
             throw new Error(response.status);
